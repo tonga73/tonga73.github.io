a3da28fe4dfa91ee1444691680caa3b47ae402bf
Gastón Mitoire <gastonmm@gmail.com>
2022-12-06 19:40:39 -0300

    fix: href contact links

--- src/features/contact/Contact.js
+++ src/features/contact/Contact.js
@@ -37,23 +37,51 @@ export default function Contact() {
         Contact
       </Typography>
       <List sx={{ width: "100%", color: "#101820" }}>
-        {contactList.map((e, index) => {
-          return (
-            <ListItem
-              button
-              key={index}
-              sx={{
-                textAlign: "right",
-                gap: 1,
-              }}
-            >
-              <ListItemText primary={e.content} />
-              <ListItemIcon sx={{ color: "#101820", minWidth: "auto" }}>
-                {e.icon}
-              </ListItemIcon>
-            </ListItem>
-          );
-        })}
+        <ListItem
+          component="a"
+          href="tel:+54 3754 463051"
+          button
+          alt="Call"
+          sx={{
+            textAlign: "right",
+            gap: 1,
+          }}
+        >
+          <ListItemText primary="+54 3754 463051" />
+          <ListItemIcon sx={{ color: "#101820", minWidth: "auto" }}>
+            <Smartphone />
+          </ListItemIcon>
+        </ListItem>
+        <ListItem
+          component="a"
+          href="mailto:gastonmm@gmail.com"
+          button
+          alt="Call"
+          sx={{
+            textAlign: "right",
+            gap: 1,
+          }}
+        >
+          <ListItemText primary="gastonmm@gmail.com" />
+          <ListItemIcon sx={{ color: "#101820", minWidth: "auto" }}>
+            <Email />
+          </ListItemIcon>
+        </ListItem>
+        <ListItem
+          component="a"
+          href="https://tonga73.github.io/"
+          button
+          alt="Call"
+          sx={{
+            textAlign: "right",
+            gap: 1,
+          }}
+        >
+          <ListItemText primary="https://tonga73.github.io/" />
+          <ListItemIcon sx={{ color: "#101820", minWidth: "auto" }}>
+            <Language />
+          </ListItemIcon>
+        </ListItem>
       </List>
     </>
   );
